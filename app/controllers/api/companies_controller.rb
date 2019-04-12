ec6b1e95e0817cd1ba55625dc7923f10fc8aee66
laugin <laugin22@gmail.com>
2019-04-12 15:23:13 +0800

    完善增删该查接口

--- app/controllers/api/companies_controller.rb
+++ app/controllers/api/companies_controller.rb
@@ -3,7 +3,30 @@ module Api
     def index
       data = Company.all.order("created_at DESC")
       data = data.search(params[:search]).order("created_at DESC") if params[:search].present?
+      data = data.where(company_params).order("created_at DESC") if company_params.present?
       render_data paginated(data), serialize_option
+    end
+
+    def show
+      data = Company.find(params[:id])
+      render_data data, serialize_option
+    end
+
+    def create
+      data = Company.create(company_params)
+      render_data data, serialize_option
+    end
+
+    def update
+      data = Company.find(params[:id])
+      data.update(company_params)
+      render_data data, serialize_option
+    end
+
+    def destroy
+      data = Company.find(params[:id])
+      data.destroy
+      render_success
     end
 
     def company_params
