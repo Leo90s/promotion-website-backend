9a94cff6795070f1ecad93c6a4ff74a6ba9e8fe5
laugin <laugin22@gmail.com>
2019-04-15 17:06:52 +0800

    修改20190414134619

--- db/migrate/20190414134619_remove_image_from_companies.rb
+++ db/migrate/20190414134619_remove_image_from_companies.rb
@@ -1,6 +1,5 @@
 class RemoveImageFromCompanies < ActiveRecord::Migration[5.2]
   def change
-    remove_column :companies, :images, :json
     add_column :companies, :images, :text, array: true, default: []
   end
 end
