cffddf4d53c6ffbc3db98bde83cc2d34035cc40c
laugin <laugin22@gmail.com>
2019-04-16 18:43:52 +0800

    删除关联字段

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -6,11 +6,6 @@
 #  file       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  company_id :bigint(8)
-#
-# Indexes
-#
-#  index_pictures_on_company_id  (company_id)
 #
 
 class Picture < ApplicationRecord
--- app/serializers/picture_serializer.rb
+++ app/serializers/picture_serializer.rb
@@ -6,15 +6,10 @@
 #  file       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  company_id :bigint(8)
-#
-# Indexes
-#
-#  index_pictures_on_company_id  (company_id)
 #
 
 class PictureSerializer < ActiveModel::Serializer
   attributes :id, :file
 
-  belongs_to :company
+  has_many :companies
 end
