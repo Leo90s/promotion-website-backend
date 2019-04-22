e1ffbd94119024d1790c98a58a11641923c91e78
laugin <laugin22@gmail.com>
2019-04-22 13:51:20 +0800

    修改迁移20190415072304

--- db/migrate/20190415072304_add_company_id_to_pictures.rb
+++ db/migrate/20190415072304_add_company_id_to_pictures.rb
@@ -1,5 +1,6 @@
 class AddCompanyIdToPictures < ActiveRecord::Migration[5.2]
   def change
+    remove_reference :picture, :company, index: true
     add_reference :pictures, :company, index: true
   end
 end
