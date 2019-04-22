0964418f2118dba69a2d5c4443472b1b06275ab2
laugin <laugin22@gmail.com>
2019-04-22 12:39:26 +0800

    修改部署生产环境的ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "173.248.240.193", user: "root", roles: %w{app db web}
+server "47.52.240.33", user: "root", roles: %w{app db web}
 
 
 
