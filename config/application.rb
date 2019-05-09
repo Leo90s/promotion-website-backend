653b0deb880fce000949d9965d528a48a125b89b
laugin <laugin22@gmail.com>
2019-05-09 17:25:15 +0800

    修改配置文件-跨域请求

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module PromotionWebsite
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :options]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
       end
     end
 
